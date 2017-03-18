0ed356eed7ef79b63b178b5d4b49f821e045ccf8
karmazub <3ubser@gmail.com>
2017-03-19 00:09:03 +0200

    add files

---
+++ README.md
@@ -0,0 +1,34 @@
+# Что в сборке
+
+<ul>
+  <li>Gulp</li>
+  <li>Bootstrap 4 сетка</li>
+</ul>
+
+# Установка
+Устанавливаем все через npm:
+
+```bash
+npm install
+```
+
+Запускаем gulp:
+
+```bash
+gulp
+```
+
+Используемые плагины:
+
+```json
+	"gulp": "^3.9.1",
+	"gulp-autoprefixer": "^3.1.1",
+	"gulp-clean-css": "^3.0.3",
+	"gulp-imagemin": "^3.1.1",
+	"gulp-jade": "^1.1.0",
+	"gulp-plumber": "^1.1.0",
+	"gulp-rename": "^1.2.2",
+	"gulp-sass": "^3.1.0",
+	"gulp-watch": "^4.3.11",
+	"gulp-webserver": "^0.9.1"
+```
---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+'use strict';
+var gulp =         require('gulp'),
+		jade =         require('gulp-jade'),
+		watch =        require('gulp-watch'),
+		sass =         require('gulp-sass'),
+		plumber =      require('gulp-plumber'),
+		autoprefixer = require('gulp-autoprefixer'),
+		imagemin =     require('gulp-imagemin'),
+		webserver =    require('gulp-webserver'),
+		rename =       require("gulp-rename"),
+		cleanCSS =     require('gulp-clean-css');
+
+
+
+var sourceDir = './src/',
+		distributionDir = './build/';
+
+gulp.task('webserver', function () {
+	gulp.src('./')
+		.pipe(webserver({
+			livereload: true,
+			directoryListing: true,
+			open: true
+		}));
+});
+
+gulp.task('jade', function () {
+	var YOUR_LOCALS = {};
+
+	gulp.src([sourceDir + 'jade/**/*.jade', '!' + sourceDir + 'jade/**/_*.jade'])
+		.pipe(jade({
+			locals: YOUR_LOCALS,
+			pretty: true
+		}))
+		.pipe(plumber())
+		.pipe(gulp.dest(distributionDir + 'pages'))
+});
+
+gulp.task('sass', function () {
+	return gulp.src([sourceDir + 'scss/**/*.scss', '!' + sourceDir + 'scss/**/_*.scss'])
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 15 versions'],
+			cascade: false
+		}))
+		.pipe(plumber())
+		.pipe(gulp.dest(distributionDir + 'css'))
+		.pipe(cleanCSS({
+			compatibility: 'ie8'
+		}))
+		.pipe(rename({
+			suffix: ".min",
+			extname: ".css"
+		}))
+		.pipe(gulp.dest(distributionDir + 'css'));
+});
+
+gulp.task('imagemin', function () {
+	gulp.src(sourceDir + 'img/**/*.*') //img/**/*.+(png|jpg|gif|svg)
+		.pipe(imagemin())
+		.pipe(gulp.dest(distributionDir + 'img'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch(sourceDir + 'jade/**/*.jade', ['jade'])
+	gulp.watch(sourceDir + 'scss/**/*.scss', ['sass'])
+});
+
+gulp.task('default', ['imagemin', 'sass', 'jade', 'watch', 'webserver']);
